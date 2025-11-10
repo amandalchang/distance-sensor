251a6f38ed2870648a2b60dec544f035026f2f01
Amanda Chang <achang2@olin.edu>
2025-11-10 18:26:47 -0500

    writing basic LED logic

--- proximity_sensor_cent/main/calibration.c
+++ proximity_sensor_cent/main/calibration.c
@@ -10,42 +10,34 @@ Calibration file for RSSI to meters
 #include <stdio.h>
 
 // define pins
-#define BUTTON 2
+#define BUTTON_PIN 4
 #define LED 5
 
 // https://randomnerdtutorials.com/esp-idf-esp32-gpio-inputs/
-#define BUTTON_PIN 4
 
-gpio_config_t io_conf = {
-    .pin_bit_mask = (1ULL << BUTTON_PIN),   // Select GPIO 4
-    .mode = GPIO_MODE_INPUT,                  // Set as input
-    .pull_up_en = GPIO_PULLUP_ENABLE,     // Enable internal pull-up
-    .pull_down_en = GPIO_PULLDOWN_DISABLE, // Disable pull-down
-    .intr_type = GPIO_INTR_DISABLE        // Disable interrupts
-};
-gpio_config(&io_conf);
-
-// create gpio button
-button_config_t gpio_btn_cfg = {
-    .type = BUTTON_TYPE_GPIO,
-    .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
-    .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
-    .gpio_button_config =
-        {
-            .gpio_num = 0,
-            .active_level = 0,
-        },
-};
-button_handle_t calibration_btn = iot_button_create(&gpio_btn_cfg);
-if (NULL == gpio_btn) {
-  ESP_LOGE(TAG, "Button create failed");
-}
+// gpio_config_t io_conf = {
+//     .pin_bit_mask = (1ULL << BUTTON_PIN),   // Select GPIO 4
+//     .mode = GPIO_MODE_INPUT,                  // Set as input
+//     .pull_up_en = GPIO_PULLUP_ENABLE,     // Enable internal pull-up
+//     .pull_down_en = GPIO_PULLDOWN_DISABLE, // Disable pull-down
+//     .intr_type = GPIO_INTR_DISABLE        // Disable interrupts
+// };
+// gpio_config(&io_conf);
 
 int main(void) {
-    gpio_get_level(BUTTON_PIN);
+    // configure
+    gpio_reset_pin(BUTTON_PIN);
+    gpio_reset_pin(LED);
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
 
   while (1) {
-    int curr_button_state = gpio_get_level(calibration_btn);
+    // int curr_button_state = gpio_get_level(calibration_btn);
+    if (gpio_get_level(BUTTON_PIN)) {
+        gpio_set_level(LED, 1);
+    } else {
+        gpio_set_level(LED, 0);
+    }
   }
 }
 
