3ada7bfe27ee0f40e85c095a478e61c3c3376019
Amanda Chang <achang2@olin.edu>
2025-12-04 04:37:11 -0500

    modified rssi distance to be calibrated every 10 inches instead of every meter, cut fluff

--- proximity_sensor_cent/main/calculation.c
+++ proximity_sensor_cent/main/calculation.c
@@ -53,9 +53,9 @@ void rssi_logreg_to_params(const int num_dists, const float rssi_array[], float*
     float log_distances[num_dists]; // storage for log distances
     float r = 0;
     
-    // assuming 1 meter apart from each measurement
+    // 10 inches between each calibration unit, eventual distance in inches
     for (int i = 0; i < num_dists; i+=1) {
-        log_distances[i] = log10(i+1);
+        log_distances[i] = log10(i+(i+1)*10);
         printf("log %i is %.3f\n", i+1, log_distances[i]);
         printf("for the %ith element, rssi = %f\n", (i+1), rssi_array[i]);
     };
@@ -64,77 +64,24 @@ void rssi_logreg_to_params(const int num_dists, const float rssi_array[], float*
 }
 
 
-// int main(void) {
-//     const int num_dists = 5; // number of measurements
-//     float m = 0; // stores slope
-//     float b = 0; // stores y-intercept
-//     float r = 0;
-//     float distances[num_dists]; // fills in calculated distances
+int main(void) {
+    const int num_dists = 10; // number of measurements
+    float m = 0; // stores slope
+    float b = 0; // stores y-intercept
 
-//     // initializing sample rssi values
-//     // const float rssi_array[5] = {-5.000, 5.602, 5.954, 6.204, 6.398};
-//     // const float rssi_array[5] = {-2.0, -3.51, -4.39, -5.01, -5.49};
-//     const float rssi_array[5] = {-56.5, -61.08, -57.9, -61.58, -58.25};
-//     rssi_logreg_to_params(num_dists, rssi_array, &m, &b);
+    const float rssi_array[10] = {-46.766666, -51.566666, -53.033333, -55.799999, -54.233334, -56.099998, -55.933334, -60.833332, -60.833332, -60.066666};
+    rssi_logreg_to_params(num_dists, rssi_array, &m, &b);
+    printf("m = %f, b = %f\n", m, b);
 
-//     // convert rssi to distances using calculated equation
-//     for (int i = 0; i < num_dists; i+=1) {
-//         float distance = rssi_to_dist(rssi_array[i], m, b);
-//         printf("The original distance was %i and the calculated is %0.4f\n", i+1, distance);
-//     };
+    // convert rssi to distances using calculated equation
+    float distance;
+    for (int i = 0; i < num_dists; i+=1) {
+        distance = rssi_to_dist(rssi_array[i], m, b);
+        printf("The original distance was %i and the calculated is %0.4f\n", i+(i+1)*10, distance);
+    };
 
-//     // save calculated logarithmic parameters
-//     FILE *f = fopen("rssi_params.txt", "w");
-//     fprintf(f, "%0.3f %0.3f\n", m, b);
-//     fclose(f);
-// }
-
-
-// /*
-// Calibration file for RSSI to meters
-// */
-
-// #include "driver/gpio.h"
-// #include "freertos/FreeRTOS.h"
-// #include "freertos/task.h"
-// #include "iot_button.h"
-// #include "sdkconfig.h"
-// #include <stdio.h>
-
-// // define pins
-// #define BUTTON_PIN 4
-// #define LED 5
-
-// // https://randomnerdtutorials.com/esp-idf-esp32-gpio-inputs/
-
-// // gpio_config_t io_conf = {
-// //     .pin_bit_mask = (1ULL << BUTTON_PIN),   // Select GPIO 4
-// //     .mode = GPIO_MODE_INPUT,                  // Set as input
-// //     .pull_up_en = GPIO_PULLUP_ENABLE,     // Enable internal pull-up
-// //     .pull_down_en = GPIO_PULLDOWN_DISABLE, // Disable pull-down
-// //     .intr_type = GPIO_INTR_DISABLE        // Disable interrupts
-// // };
-// // gpio_config(&io_conf);
-
-// int main(void) {
-//     // configure
-//     gpio_reset_pin(BUTTON_PIN);
-//     gpio_reset_pin(LED);
-//     gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
-//     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
-
-//     if (gpio_get_level(BUTTON_PIN)) {
-//       // start 10
-//       // record all rssi measurements to an array of fixed size 10
-//     }
-//     // non-blocking send the average out to a different variable
-    
-
-//   while (1) {
-//     if (gpio_get_level(BUTTON_PIN)) {
-//         gpio_set_level(LED, 1);
-//     } else {
-//         gpio_set_level(LED, 0);
-//     }
-//   }
-// }
+    // save calculated logarithmic parameters
+    FILE *f = fopen("rssi_params.txt", "w");
+    fprintf(f, "%0.3f %0.3f\n", m, b);
+    fclose(f);
+}
